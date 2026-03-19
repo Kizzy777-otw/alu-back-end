33262b5f67c5c8ad457227399517039195000725
Imena Uwizeye Kizito <i.kizito@alustudent.com>
2026-03-19 19:58:20 +0200

    pep-8 validation debug

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -22,17 +22,18 @@ def main():
         if todo['userId'] == user_id:
             total_questions += 1
 
-        if todo['completed']:
-            completed.append(todo['title'])
+            if todo['completed']:
+                completed.append(todo['title'])
 
     user_name = requests.get(user_url).json()['name']
 
-    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
-               len(completed), total_questions))
-    print(printer)
+    with open('student_output', 'w') as f:
+        printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+                   len(completed), total_questions))
+        f.write(printer + '\n')
 
-    for q in completed:
-        print("\t {}".format(q))
+        for q in completed:
+            f.write("\t {}\n".format(q))
 
 
 if __name__ == "__main__":
