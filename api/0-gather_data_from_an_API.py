2ff09f782b4322b23626f1cc6b4ec0e744d5c13d
Imena Uwizeye Kizito <i.kizito@alustudent.com>
2026-03-19 14:25:33 +0200

    total quetions var

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,20 +16,21 @@ def main():
 
     response = requests.get(todo_url)
 
-    total_tasks = 0
+    total_questions = 0
     completed = []
     for todo in response.json():
-        if todo["userId"] == user_id:
-            total_tasks += 1
-            if todo["completed"]:
-                completed.append(todo["title"])
+            if todo['userId'] == user_id:
+                total_questions += 1
 
-    user_name = requests.get(user_url).json()["name"]
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user_name, len(completed), total_tasks
-        )
-    )
+            if todo['completed']:
+                completed.append(todo['title'])
+
+    user_name = requests.get(user_url).json()['name']
+
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
+
     for q in completed:
         print("\t {}".format(q))
 
