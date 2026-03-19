8a2b068bfe95556ee584a033753910f3d7a9ad5b
Imena Uwizeye Kizito <i.kizito@alustudent.com>
2026-03-19 20:15:38 +0200

    checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python3
-"""Script to get TODO progress for an employee from API."""
-
+"""
+Python script that returns TODO list progress for a given employee ID
+"""
 import requests
-import sys
+from sys import argv
 
 
-def main():
-    """Main function."""
-    if len(sys.argv) != 2:
-        return
+def get_employee_info(employee_id):
+    """
+    Get employee information by employee ID
+    """
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/'
+    response = requests.get(url)
+    return response.json()
 
-    user_id = int(sys.argv[1])
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    response = requests.get(todo_url)
+def get_employee_todos(employee_id):
+    """
+    Get the TODO list of the employee by employee ID
+    """
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(url)
+    return response.json()
 
-    total_questions = 0
-    completed = []
-    for todo in response.json():
-        if todo['userId'] == user_id:
-            total_questions += 1
 
-            if todo['completed']:
-                completed.append(todo['title'])
+def main(employee_id):
+    """
+    Main function to fetch and display the TODO list progress of the employee
+    """
+    employee = get_employee_info(employee_id)
+    employee_name = employee.get("name")
 
-    user_name = requests.get(user_url).json()['name']
+    emp_todos = get_employee_todos(employee_id)
+    tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
 
-    with open('student_output', 'w') as f:
-        printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
-                   len(completed), total_questions))
-        f.write(printer + '\n')
+    total_tasks = len(tasks)
+    completed_tasks = [completed for completed in tasks.values() if completed]
+    completed_tasks_count = len(completed_tasks)
 
-        for q in completed:
-            f.write("\t {}\n".format(q))
+    print(f"Employee {employee_name} is done with tasks"
+          f"({completed_tasks_count}/{total_tasks}):")
+    for title, completed in tasks.items():
+        if completed:
+            print(f"\t {title}")
 
 
 if __name__ == "__main__":
-    main()
+    if len(argv) > 1:
+        main(argv[1])
+    else:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
